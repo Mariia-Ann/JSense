9c3dcf1f6fe80585f7198657b1551388b824f85a
Tetiana Savchuk <tanyainfo86@gmail.com>
2025-03-10 23:28:17 +0200

    finish

--- src/js/faq.js
+++ src/js/faq.js
@@ -1,36 +1,31 @@
 import Accordion from "accordion-js";
 import "accordion-js/dist/accordion.min.css";
+
 document.addEventListener('DOMContentLoaded', () => {
   
-    const faqItems = document.querySelectorAll('.faq-item');
- 
-
-    function closeAll() {
-      faqItems.forEach(item => {
-        item.querySelector('.faq-text').classList.add('hidden'); 
-        item.querySelector('.faq-vector-icon').classList.remove('open'); 
-      });
-    }
- 
-    faqItems.forEach(item => {
-      const toggleButton = item.querySelector('.faq-container');
- 
-      toggleButton.addEventListener('click', () => {
-        const answer = item.querySelector('.faq-text');
-        const icon = item.querySelector('.faq-vector-icon');
- 
-        if (!answer.classList.contains('hidden')) {
-          answer.classList.add('hidden');
-          icon.classList.remove('open');
-        } else {
-          closeAll();
- 
-          answer.classList.remove('hidden');
-          icon.classList.add('open');
-        }
-      });
-    });
- 
-    closeAll();
+  // Створення екземпляра Accordion
+  const accordion = new Accordion('.faq-item', {
+    openOnInit: [] // Не відкриваємо нічого по замовчуванню
   });
 
+  const faqItems = document.querySelectorAll('.faq-item');
+  
+  // Тепер ми не будемо закривати всі секції при натисканні
+  faqItems.forEach(item => {
+    const toggleButton = item.querySelector('.faq-container');
+    
+    toggleButton.addEventListener('click', () => {
+      const answer = item.querySelector('.faq-text');
+      const icon = item.querySelector('.faq-vector-icon');
+      
+      // Перевірка, чи потрібно показати/сховати відповідь
+      if (answer.classList.contains('hidden')) {
+        answer.classList.remove('hidden');
+        icon.classList.add('open');
+      } else {
+        answer.classList.add('hidden');
+        icon.classList.remove('open');
+      }
+    });
+  });
+})
