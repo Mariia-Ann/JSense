be53cc39a3e2b84cae72e3d30ea1ffc8af3e31e4
Tetiana Savchuk <tanyainfo86@gmail.com>
2025-03-13 10:15:54 +0200

    fixbag

--- src/js/faq.js
+++ src/js/faq.js
@@ -1,31 +1,34 @@
 import Accordion from "accordion-js";
 import "accordion-js/dist/accordion.min.css";
-
-document.addEventListener('DOMContentLoaded', () => {
-
-
-  const accordion = new Accordion('.faq-item', {
+document.addEventListener("DOMContentLoaded", () => {
+  const accordion = new Accordion(".faq-item", {
     openOnInit: []
   });
 
-  const faqItems = document.querySelectorAll('.faq-item');
+  const faqItems = document.querySelectorAll(".faq-item");
 
+  faqItems.forEach((item) => {
+    const toggleButton = item.querySelector(".faq-container");
+    const answer = item.querySelector(".faq-text");
+    const icon = item.querySelector(".faq-vector-icon");
 
-  faqItems.forEach(item => {
-    const toggleButton = item.querySelector('.faq-container');
-
-    toggleButton.addEventListener('click', () => {
-      const answer = item.querySelector('.faq-text');
-      const icon = item.querySelector('.faq-vector-icon');
-
-
-      if (answer.classList.contains('hidden')) {
-        answer.classList.remove('hidden');
-        icon.classList.add('open');
-      } else {
-        answer.classList.add('hidden');
-        icon.classList.remove('open');
-      }
+    toggleButton.addEventListener("click", () => {
+      const isOpen = item.classList.contains("active");
+      toggleFAQ(item, answer, icon, !isOpen);
     });
   });
-})
+
+  function toggleFAQ(item, answer, icon, open) {
+    if (open) {
+      item.classList.add("active");
+      answer.style.maxHeight = answer.scrollHeight + "px";
+      answer.style.opacity = "1";
+      icon.style.transform = "rotate(180deg)";
+    } else {
+      item.classList.remove("active");
+      answer.style.maxHeight = "0";
+      answer.style.opacity = "0";
+      icon.style.transform = "rotate(0deg)";
+    }
+  }
+});
